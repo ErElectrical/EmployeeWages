587579f9208c983895c848831e70bffd42ef086e
ErElectrical <94623283+ErElectrical@users.noreply.github.com>
2021-12-18 18:34:43 +0000

    Add files via upload

---
+++ emp_present_ornot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace employee
+{
+    public class Employee
+    {
+        static void Main(string[] args)
+        {
+            int emp_check = 1;
+            Random r = new Random();    
+            int outr = r.Next(0, 2);
+            if ( outr == emp_check)
+            {
+                Console.WriteLine("employee prsent");
+            }
+            else
+            {
+                Console.WriteLine("employee not present");
+            }
+        }
+    }
+}
+
