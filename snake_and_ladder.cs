f27c5d1a3cf4b6093e2014ffb571a44e3c06e987
ErElectrical <94623283+ErElectrical@users.noreply.github.com>
2021-12-18 18:46:33 +0000

    Add files via upload

---
+++ snake_and_ladder.cs
@@ -0,0 +1,62 @@
+﻿class program
+{
+    public int sum = 0;
+    public int count = 0;
+    public int random()
+    {
+        Random random = new Random();
+        int check = random.Next(1, 7);
+        count++;
+        return check;
+
+    }
+    public  void game_methology()
+    {
+        while (sum == 100)
+        {
+            int opt = random();
+            if (opt == 0)
+            {
+                sum += 0;
+                Console.WriteLine("no play");
+            }
+            else if (opt == 3 || opt == 6)
+            {
+                if (sum - opt < 0)
+                {
+                    sum = 0;
+                }
+                else
+                {
+                    sum -= opt;
+                    Console.WriteLine("snake bite ");
+
+
+
+                }
+            }
+            else if (opt == 2 || opt == 4 || opt == 5)
+            {
+                if (sum + opt > 100)
+                {
+                    sum += 0;
+                }
+                else
+                {
+                    sum += opt;
+                    Console.WriteLine("ladder achieve");
+
+
+
+                }
+            }
+        }
+        Console.WriteLine("user won in ---  " + count + " trial ");      
+
+        
+    }
+
+
+    
+
+}
