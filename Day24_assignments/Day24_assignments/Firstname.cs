48d488ce57f9759ac2eb2c8b47f17379222199a6
ErElectical <mohittanwar786.mt@gmail.com>
2022-01-14 05:05:14 +0000

    [Add]Mohit validate user input

---
+++ Day24_assignments/Day24_assignments/Firstname.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+namespace Day24_assignments
+{
+     class pattern
+    {
+        // charcter class 
+
+        //. --------- Match any chacrcter except new line
+        // \d ----- Digit(0-9)
+        // \D ----- Not a digit(0-9)
+        // \w ------ word charcter (a-z ,A-Z,0-9,_)
+        // \W ------ Not a word chacter 
+        // \s ------ whitespace,new Line
+        // \S ------- Not whitespace 
+        // [] --------- Matches charcter in bracket
+        // [^ ] ------- Matches charcter not in bracket
+        // | ------ Either or
+        // () ---- group
+        // \b ---- work boundary(means space)
+        // \B ---- Not a work boundary
+        // ^ ---- Begining of a string
+        // $ ---- ending of string
+
+        // Quantifier
+
+        // * -------- 0 or More
+        // + -------- 1 or more
+        // ? -------- 0 or 1
+        // {3} ------ only 3
+        // {3,7} ----- bw 3 and 7.
+
+        public static void Check(string pat, string input)
+        {
+            Regex rxName = new Regex(pat);
+            bool result = rxName.IsMatch(input);
+            if (result == false)
+            {
+                Console.WriteLine("not valid");
+                Console.WriteLine(" read manual");
+
+            }
+            else
+            {
+                Console.WriteLine("valid");
+
+            }
+
+        }
+
+        public void ValidFirstname()
+        {
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+            string rx = @"^[A-Z]{1}[a-z]{3,}$";
+            pattern.Check(rx, name);
+        }
+
+        public void ValidateLastName()
+        {
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+            string rx = @"^[A-Z]{1}[a-z]{3,}$";
+            pattern.Check(rx, name);
+        }
+
+        public void ValidEmailid()
+        {
+            Console.WriteLine("enter Email ");
+            string name = Console.ReadLine();
+            string rx = @"^[a-zA-Z0-9]+([.][A-Za-z0-9]+)?[@][\w]{2, }[.][a-zA-Z]{2,3}[.][a-zA-Z]{2, }?$";
+            pattern.Check(rx, name);
+        }
+       
+        public void Phonenumber()
+        {
+            Console.WriteLine("enter phonenumber ");
+            string name = Console.ReadLine();
+            string rx = @"^ [1-9]{2,}\s[6-9][0-9]{9}$";
+            pattern.Check(rx, name);
+
+        }
+
+        public void Password()
+        {
+            Console.WriteLine("enter password ");
+            string name = Console.ReadLine();
+            string rx = @"^[a-zA-Z]{8,}$";
+            pattern.Check(rx, name);
+
+        }
+
+        public void Password1()
+        {
+            Console.WriteLine("enter password ");
+            string name = Console.ReadLine();
+            string rx = "^((?=.*[A-Z])(?=.{8,}))";
+            pattern.Check(rx, name);
+
+        }
+
+        public void Password2()
+        {
+            Console.WriteLine("enter password ");
+            string name = Console.ReadLine();
+            string rx = "^((?=.*[!@#$%^&*~])[A-Za-z]{8,}$";
+            pattern.Check(rx, name);
+
+        }
+
+        public void EmailSample()
+        {
+            Console.WriteLine("enter Emails for sample ");
+            string name = Console.ReadLine();
+            string rx = "^[a-zA-Z0-9-.+]{3,}[@][a-z]{3,5}[.][com|au|net|com.com|com.net|com.au]$";
+            pattern.Check(rx, name);
+
+        }
+
+    }
+}
---
+++ Day24_assignments/Day24_assignments/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Day24_assignments
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to Regex pattern ");
+
+            pattern p= new pattern();
+            Console.WriteLine("enter what you want to validate ");
+            Console.WriteLine("1. firstName " +
+                "\n 2. Lastname " +
+                "\n 3. Emailid " +
+                "\n 4. PhoneNumber " +
+                "\n 5. password rule 1 " +
+                "\n 6. Password rule 2 " +
+                "\n 7. password rule 3 " +
+                "\n 8. Email sample ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    {
+                        p.ValidFirstname();
+                        break;
+                    }
+                case 2:
+                    {
+                        p.ValidateLastName();
+                        break;
+                    }
+                case 3:
+                    {
+                        p.ValidEmailid();
+                        break;
+                    }
+                case 4:
+                    {
+                        p.Phonenumber();
+                        break;
+                    }
+                case 5:
+                    {
+                        p.Password();
+                        break;
+                    }
+                case 6:
+                    {
+                        p.Password1();
+                        break;
+                    }
+                case 7:
+                    {
+                        p.Password2();
+                        break;
+                    }
+                case 8:
+                    {
+                        p.EmailSample();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("read again and choice correct ");
+                        break;
+                    }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
